296e3a84a4f24a4fb57246250eec97e4edf0c525
Tue Vu <vu.tue.1710@gmail.com>
2017-09-08 17:30:10 +0700

    MASTER BRANCH
    my first commit

---
+++ app/containers/App.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react'
+import {Switch,Route,Router} from 'react-router-dom'
+
+class App extends Component {
+    constructor(props){
+        super(props)
+    }
+
+    render() {
+        return(
+            <div>
+                <h2>
+                    HELLO REACT
+                </h2>
+            </div>
+        )
+    }
+}
+
+export default App
